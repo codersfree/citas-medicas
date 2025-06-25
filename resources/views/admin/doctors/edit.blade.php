93dfa6d88a971bfed90ea3f5f2c7431e069cc93f
Victor Arana <victor@codersfree.com>
2025-06-25 01:27:56 -0500

    rutas para doctores

---
+++ resources/views/admin/doctors/edit.blade.php
@@ -0,0 +1,17 @@
+<x-admin-layout
+title="Doctores | Coders Free"
+:breadcrumbs="[
+    [
+        'name' => 'Dashboard',
+        'href' => route('admin.dashboard'),
+    ],
+    [
+        'name' => 'Doctores',
+        'href' => route('admin.doctors.index'),
+    ],
+    [
+        'name' => 'Editar'
+    ]
+]">
+
+</x-admin-layout>
---
+++ resources/views/admin/doctors/index.blade.php
@@ -0,0 +1,13 @@
+<x-admin-layout
+title="Doctores | Coders Free"
+:breadcrumbs="[
+    [
+        'name' => 'Dashboard',
+        'href' => route('admin.dashboard'),
+    ],
+    [
+        'name' => 'Doctores'
+    ]
+]">
+
+</x-admin-layout>
